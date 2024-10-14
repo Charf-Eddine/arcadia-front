76cca293b42df6b180bd0577b00fb13bd7b4e415
Charf-Eddine <habibcharfeddine@yahoo.fr>
2024-10-14 16:05:10 +0200

    Authentification dans le routage

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,9 +2,9 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/signin", "Connexion", "/pages/signin.html","js/auth/signin.js"),
-    new Route("/contact-us", "Contactez-nous", "/pages/contact-us.html","js/contact-us.js")
+    new Route("/", "Accueil", "/pages/home.html", []),
+    new Route("/signin", "Connexion", "/pages/signin.html", ["disconnected"],"/js/auth/signin.js"),
+    new Route("/contact-us", "Contactez-nous", "/pages/contact-us.html", [],"/js/contact-us.js")
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
