09fb80889d5f2d0e1f4d35bb58bcd282dab38a81
Charf-Eddine <habibcharfeddine@yahoo.fr>
2024-10-16 00:28:25 +0200

    Page Services

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,6 +3,7 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html", []),
+    new Route("/services", "Services", "/pages/services.html", [],"/js/services.js"),
     new Route("/signin", "Connexion", "/pages/signin.html", ["disconnected"],"/js/auth/signin.js"),
     new Route("/contact-us", "Contactez-nous", "/pages/contact-us.html", [],"/js/contact-us.js"),
     new Route("/schedules", "Nos horaires", "/pages/schedules.html", []),
