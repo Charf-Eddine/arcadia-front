ebbfe624df73af6e224835638df2bcbc2ecd421d
Charf-Eddine <habibcharfeddine@yahoo.fr>
2024-10-15 23:49:11 +0200

    Mettre les horaires dans une page à part + amélioration du footer

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,7 +4,8 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html", []),
     new Route("/signin", "Connexion", "/pages/signin.html", ["disconnected"],"/js/auth/signin.js"),
-    new Route("/contact-us", "Contactez-nous", "/pages/contact-us.html", [],"/js/contact-us.js")
+    new Route("/contact-us", "Contactez-nous", "/pages/contact-us.html", [],"/js/contact-us.js"),
+    new Route("/schedules", "Nos horaires", "/pages/schedules.html", []),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
